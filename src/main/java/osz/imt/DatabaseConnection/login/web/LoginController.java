e5a3f4faf207f19a634e7babef057400b01f06fe
Janek von Lipisnki <janekvonlipinski222@gmail.com>
2025-06-29 10:56:18 +0200

    adjusted statuscodes

--- src/main/java/osz/imt/DatabaseConnection/login/web/LoginController.java
+++ src/main/java/osz/imt/DatabaseConnection/login/web/LoginController.java
@@ -1,6 +1,8 @@
 package osz.imt.DatabaseConnection.login.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import osz.imt.DatabaseConnection.login.service.LoginService;
 import osz.imt.DatabaseConnection.login.web.model.Login;
@@ -13,12 +15,31 @@ public class LoginController {
     private final LoginService loginService;
 
     @GetMapping("/login")
-    public Login login(@ModelAttribute  Login login) {
-        return loginService.login(login);
+    public ResponseEntity<?> login(@ModelAttribute  Login login) {
+
+        Login attemptedLogin;
+        try {
+            attemptedLogin = loginService.login(login);
+        } catch (RuntimeException r) {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        }
+
+        if (attemptedLogin == null) {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(401));
+        }
+
+        return new ResponseEntity<>(attemptedLogin, HttpStatusCode.valueOf(200));
     }
 
     @PostMapping("/login")
-    public Login createAccount(@RequestBody  Login login) {
-        return loginService.createAccount(login);
+    public ResponseEntity<?> createAccount(@RequestBody Login login) {
+        Login createdLogin;
+        try {
+            createdLogin = loginService.createAccount(login);
+        } catch (RuntimeException r) {
+            return new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        }
+
+        return new ResponseEntity<>(createdLogin, HttpStatusCode.valueOf(201));
     }
 }
